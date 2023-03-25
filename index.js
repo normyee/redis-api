ed80e10f88f27b40236e85e186446bbac3c380e8
normyee <lunoqro@gmail.com>
2023-03-24 21:22:10 -0300

    redis project files

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const redis = require("redis");
+
+const client = redis.createClient();
+
+
+app.get("/rockets", async(req, res) => {
+      try {
+        const rocketsCache = await client.get("myRockets");
+        if (rocketsCache) {
+            return res.send(JSON.parse(rocketsCache));
+        }
+        const allRockets = await axios.get("https://api.spacexdata.com/v3/rockets");
+        await client.set("myRockets", JSON.stringify(allRockets.data), {EX: 5});
+        res.status(200).send(allRockets.data);
+
+    } catch(e){
+        res.status(400).json({status: "Error", message: e});
+    }
+
+});
+
+const startup = async() => {
+    await client.connect();
+    app.listen(3000, () => {
+        console.log("Server is on!");
+    });
+};
+startup();
